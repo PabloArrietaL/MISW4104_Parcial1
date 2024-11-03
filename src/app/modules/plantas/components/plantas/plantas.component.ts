80ac55a9f9b433bf6b7dcacfbe8ff5291c5b90ae
Pablo Arrieta <pablou2015@hotmail.com>
2024-11-03 12:21:46 -0500

    Desarrollo Punto N2

--- src/app/modules/plantas/components/plantas/plantas.component.ts
+++ src/app/modules/plantas/components/plantas/plantas.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { map, Observable, Subscription } from 'rxjs';
 import { Planta } from '../../../../data/models/planta.model';
 import { PlantasService } from '../../../../data/services/plantas.service';
 import { AsyncPipe, NgFor } from '@angular/common';
@@ -11,14 +11,47 @@ import { AsyncPipe, NgFor } from '@angular/common';
   templateUrl: './plantas.component.html',
   styleUrl: './plantas.component.scss'
 })
-export class PlantasComponent implements OnInit {
+export class PlantasComponent implements OnInit, OnDestroy {
 
-  plantas$!: Observable<Planta[]>;
+  plantas!: Planta[];
+  contador: {interior: number, exterior: number};
+  subscription: Subscription = new Subscription();
 
-  constructor(private plantasService: PlantasService){}
+  constructor(private plantasService: PlantasService){
+    this.contador = {
+      exterior: 0,
+      interior: 0
+    };
+  }
 
   ngOnInit(): void {
-    this.plantas$ = this.plantasService.getAll();
+    this.subscription.add(this.plantasService.getAll()
+    .pipe(
+      map((plantas: Planta[]) => {
+        return plantas.reduce(
+          (acumulador, planta) => {
+          if(planta.tipo == 'Interior') {
+            acumulador.interior.push(planta);
+          } else if(planta.tipo == 'Exterior') {
+            acumulador.exterior.push(planta);
+          }
+          return acumulador;
+        }, {interior: [] as Planta[], exterior: [] as Planta[]}
+      )
+      })
+    )
+    .subscribe(
+      plantas => {
+        this.contador.exterior = plantas.exterior.length;
+        this.contador.interior = plantas.interior.length;
+        this.plantas =[...plantas.exterior, ...plantas.interior];
+
+      }
+    ));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
